f746b393e1e5f50ac4d7787584b8fc79e4429a5d
Mazhar Khan <mazhar.cse2005@gmail.com>
2023-10-06 21:20:56 +0530

    Python_Module2_Home Task1_Q#9_SquareRoot.py

---
+++ Python_Module2_HomeTask1_Q#9_SquareRoot.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter a num 1: "))
+num2 = int(input("Enter a num 2: "))
+num3 = int(input("Enter a num 3: "))
+num4 = int(input("Enter a num 4: "))
+sum = num1+num2+num3+num4
+avg = sum/4
+print("The average of "+str(num1)+", "+str(num2)+", "+str(num3)+" and "+str(num4)+" is : "+str(avg))
